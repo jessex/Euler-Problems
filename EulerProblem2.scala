06156cf44cd3756b01579637bc21aabcfa8e63a5
Joshua Essex <essexjosh@gmail.com>
2011-02-17 02:17:43 -0500

    Comments. Comments everywhere

--- EulerProblem2.scala
+++ EulerProblem2.scala
@@ -1,6 +1,6 @@
 def fibonacci(max:Int, prev:BigInt, next:BigInt):List[BigInt] = {
 	if ((prev + next) > max) List()
-	else (prev + next) :: fibonacci(max, next, prev + next)
+	else (prev + next) :: fibonacci(max, next, prev + next) //Tail-recursion!!
 }
-
+//Construct list of fibonacci #s, filter for evens, sum them up
 println(fibonacci(4000000, 0, 1).filter(_%2 == 0).reduceLeft(_+_))
--- EulerProblem3.scala
+++ EulerProblem3.scala
@@ -1,6 +1,6 @@
 def twoFactors(number:BigInt, start:Int):List[BigInt] = {
 	var factor:Int = start
-	val checker = math.sqrt(number.doubleValue)
+	val checker = math.sqrt(number.doubleValue) //Cut-off point
 	while(1 == 1) {
 		if ((number % factor) == 0) return List(factor, number/factor)
 		else factor += 1
@@ -10,17 +10,18 @@ def twoFactors(number:BigInt, start:Int):List[BigInt] = {
 	return List()
 }
 
+//Node representing combination of factors of parent
 class Node(a:BigInt, b:BigInt) {
 	var aPrime = false
 	var bPrime = false
 	
-	val aFactors = twoFactors(a, 2)
-	if (aFactors.isEmpty) aPrime = true
+	val aFactors = twoFactors(a, 2) //Get factors for factor a
+	if (aFactors.isEmpty) aPrime = true //No factors, so is prime
 	else {
-		val aChild = new Node(aFactors(0), aFactors(1))
+		val aChild = new Node(aFactors(0), aFactors(1)) //Construct child
 	}
 	
-	val bFactors = twoFactors(b, 2)
+	val bFactors = twoFactors(b, 2) //Repeat for factor b...
 	if (bFactors.isEmpty) bPrime = true
 	else {
 		val bChild = new Node(bFactors(0), bFactors(1))
@@ -33,6 +34,6 @@ class Node(a:BigInt, b:BigInt) {
 var primeFactors = List[BigInt]()
 val starters = twoFactors(BigInt("600851475143"), 2)
 if (!starters.isEmpty) {
-	val n = new Node(starters(0), starters(1))
+	val n = new Node(starters(0), starters(1)) //Kickstart tree construction
 }
 println(primeFactors.max)
